3a767327c327c48b524a0dbf8dda851677cfdbd8
unknown <881030.n@gmail.com>
2024-12-03 04:10:16 +0900

    addResuto

---
+++ Assets/GhostTimer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+
+public class GhostTimer : MonoBehaviour
+{
+    public float timeRemaining = 10f; // 初期タイマー時間
+    public TextMeshProUGUI timerText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime;
+        timerText.text = timeRemaining.ToString("0");
+
+    }
+}
